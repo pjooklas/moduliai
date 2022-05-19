f1988fa7c025ec1c51e8da523d1c8dea9468a25c
pjooklas <pjooklas@users.noreply.github.com>
2022-05-19 19:47:58 +0300

    init

---
+++ dir/fnkc.js
@@ -0,0 +1,10 @@
+function suma(a, b) {
+    return a + b;
+}
+
+function skirtumas(a, b) {
+    return a - b;
+}
+
+exports.suma = suma;
+exports.skirtumas = skirtumas;
---
+++ index.js
@@ -0,0 +1,6 @@
+const f = require("./dir/fnkc.js");
+const f1 = require("./dir/fnkc.js");
+console.log(f === f1);
+
+const rez = f.suma(1, 2);
+console.log(rez);
